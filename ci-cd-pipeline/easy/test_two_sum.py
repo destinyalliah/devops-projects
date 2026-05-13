425dec0a70ab4438dd2fb72a00fbc1ebfe217d90
destinyalliah <destinyalliahh@gmail.com>
2026-05-13 20:52:07 +0100

    Update test_two_sum.py file to import Solution correctly

--- ci-cd-pipeline/easy/test_two_sum.py
+++ ci-cd-pipeline/easy/test_two_sum.py
@@ -1,5 +1,6 @@
-from two_sum import twoSum
+from two_sum import Solution
 
 def test_two_sum():
-    assert twoSum([2, 7, 11, 15], 9) == [0, 1]
-    assert twoSum([3, 2, 4], 6) == [1, 2]
+    s = Solution()
+    assert s.twoSum([2, 7, 11, 15], 9) == [0, 1]
+    assert s.twoSum([3, 2, 4], 6) == [1, 2]
